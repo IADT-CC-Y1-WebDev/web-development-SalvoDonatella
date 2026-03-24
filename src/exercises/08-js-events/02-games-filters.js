ab0a2a94509e1ac3e59326b66be252ba5748018d
Salvo Donatella <n00256819@iadt.ie>
2026-03-24 12:56:31 +0000

    JS filters

--- src/exercises/08-js-events/02-games-filters.js
+++ src/exercises/08-js-events/02-games-filters.js
@@ -5,28 +5,58 @@ let cards = document.querySelectorAll(".card");
 
 let form = document.getElementById("filters");
 
-applyBtn.addEventListener('click', (event) => { event.preventDefault();
+applyBtn.addEventListener('click', (event) => { 
+    event.preventDefault();
     applyFilters(); 
-})
-clearBtn.addEventListener('click', (event) => {event.preventDefault();
+});
+clearBtn.addEventListener('click', (event) => {
+    event.preventDefault();
     clearFilters();
-})
+});
 
 function applyFilters(){
     //console.log("Applying filters");
     let filters = getFilters();
-    let matches = [];
+    // let matches = [];
 
     for(let i = 0; i != cards.length; i++){
         let card = cards[i];
-        matches[i] = cardMatches(card, filters);
+        match = cardMatches(card, filters);
+        card.classList.toggle('hidden', !match);
     }
+    let cardsArray = Array.from(cards);
+    const sorted = sortCards(cardsArray, filters.sortBy);
+}
+
+function sortCards(cards, sortBy) {
+    const list = cards.slice();
+
+    list.sort((a,b) => {
+    let titleA = a.dataset.title.toLowerCase();
+    let titleB = b.dataset.title.toLowerCase();
+    let yearA = Number(a.dataset.year);
+    let yearB = Number(b.dataset.year);
+
+    if (sortBy === "year_desc") return yearB - yearA;
+    if (sortBy === "year_asc") return yearA - yearB;
+
+    return titleA.localeCompare(titleB);
+    });
+
+    return list;
 }
 
 function cardMatches(crd, fltrs) {
     //console.log(crd.dataset.title, fltrs.titleFilter);
     let title = crd.dataset.title.toLowerCase();
-    return title.includes(fltrs.titleFilter);
+    let genre = crd.dataset.genre;
+    let platform = crd.dataset.platform;
+
+    let matchTitle    = fltrs.titleFilter    === "" || title.includes(fltrs.titleFilter);
+    let matchGenre    = fltrs.genreFilter    === "" || genre === fltrs.genreFilter;
+    let matchPlatform = fltrs.platformFilter === "" || platform.includes(fltrs.platformFilter);
+
+    return matchTitle && matchGenre && matchPlatform;
 }
 
 function getFilters() {
